ef9261d4f2c18d471814bf9eed4adda459007447
Gytis Balciunas <gytisbalciunas1551@gmail.com>
2019-08-11 19:33:19 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,12 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-// Route::get('/admin', 'PagesController@admin');
 
 Route::resource('users', 'UsersController');
+
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
-// Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+
 Route::get('/', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
 Route::post('/', 'Auth\AdminLoginController@login')->name('admin.login.submit');
